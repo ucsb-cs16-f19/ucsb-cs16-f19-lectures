0c7c753c527113b5126ccbb85c72d6885e2d07f9
Diba Mirza <dimirza@cs.ucsb.edu>
2019-11-14 13:25:48 -0800

    lec 14

---
+++ lec-14/linkedlist.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+/*
+For any data structure
+1. insert a value (at the front or at any location)
+2. find a value
+3. min or max
+4. print all the elements
+5. delete a value
+6. delete all the values 
+*/
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+struct LinkedList{
+	Node* head;
+	Node* tail;
+};
+
+
+//insert to the head of the linked list
+void insertNode(LinkedList& list, int value){
+	list.head = new Node{value, list.head};
+	if(!list.tail){
+		list.tail = list.head;
+	}
+
+}
+
+void printList(LinkedList& list){
+	Node* p = list.head;
+	while(p){
+		cout<<p->data<<" ";
+		p = p->next;
+	}
+	cout<<endl;
+
+}
+
+Node* find(const LinkedList& list, int value);
+
+int count(const LinkedList& list){
+	int sum = 0;
+	Node* p = list.head;
+	while(p){
+		sum+=p->data;
+		p = p->next;
+	}
+	return sum;
+}
+//delete all the nodes
+void clear(LinkedList& list){
+	Node* p = list.head;
+	while(p){
+		// Do something
+		Node* tmp = p;
+		p = p->next;
+		delete tmp;
+	}
+
+}
+
+int main(){
+	//Node* h = 0; //empty linked list
+	LinkedList ll = {0, 0}; //empty linkedlist
+	insertNode(ll, 10); //
+	insertNode(ll, 20);
+	insertNode(ll, 30);
+	insertNode(ll, 40);
+	printList(ll);
+	clear(ll);
+
+	return 0;
+}
